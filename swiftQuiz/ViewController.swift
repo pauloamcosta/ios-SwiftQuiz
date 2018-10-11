8585d1f7c2ecd5250eeb436f6e5ebc47e6c9ef1a
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2018-10-10 23:08:35 -0300

    Initial Commit

---
+++ swiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftQuiz
+//
+//  Created by Paulo Costa on 10/10/2018.
+//  Copyright © 2018 Paulo Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
