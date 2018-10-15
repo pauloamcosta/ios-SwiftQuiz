172e63855050e0101f7ac0bc755285e453b7d502
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2018-10-15 00:43:51 -0300

    creating logic

--- swiftQuiz/Quiz.swift
+++ swiftQuiz/Quiz.swift
@@ -10,5 +10,21 @@ import Foundation
 
 
 class Quiz{
+    let question: String
+    let options: [String]
+    private let correctedAnswer: String
     
+    init(question: String, options: [String], correctedAnswer: String){
+        self.question = question
+        self.options = options
+        self.correctedAnswer = correctedAnswer
+    }
+    
+    func validateOptions(_ index: Int) -> Bool{
+        let answer = options[index]
+        return answer == correctedAnswer
+    }
+    deinit {
+        print("Liberou quiz da memória")
+    }
 }
