53470700d26577d77e00b6f95d3d4d6e0be18fa3
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2018-10-16 22:42:18 -0300

    running with bugs. need fixes on screens

--- swiftQuiz/QuizManager.swift
+++ swiftQuiz/QuizManager.swift
@@ -61,7 +61,18 @@ class QuizManager{
         return _totalAnswers
     }
     var totalCorrectAnswers: Int{
-        return totalCorrectAnswers
+        return _totalCorrectAnswers
+    }
+    func refreshQuiz(){
+        let randomIndex = Int(arc4random_uniform(UInt32(quizes.count)))
+        let quizData = quizes[randomIndex]
+        quiz = Quiz(question: quizData.question, options: quizData.options, correctedAnswer: quizData.correctAnswer)
+    }
+    func validateAnswer(index: Int){
+        _totalAnswers += 1
+        if quiz.validateOptions(index){
+            _totalCorrectAnswers += 1
+        }
     }
 
 }
--- swiftQuiz/QuizViewController.swift
+++ swiftQuiz/QuizViewController.swift
@@ -14,24 +14,45 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var lbQuestion: UILabel!
     @IBOutlet var btAnswers: [UIButton]!
     
+    let quizManager = QuizManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        viTimer.frame.size.width = view.frame.size.width
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.viTimer.frame.size.width = 0
+        }) {(success) in
+            self.showResults()
+        }
+        getNewQuiz()
+    }
+    func getNewQuiz(){
+        quizManager.refreshQuiz()
+        lbQuestion.text = quizManager.question
+        for i in 0..<quizManager.options.count{
+            let option = quizManager.options[i]
+            let button = btAnswers[i]
+            button.setTitle(option, for: .normal)
+        }
+    }
+    func showResults(){
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewcontroller = segue.destination as! ResultViewController
+        resultViewcontroller.totalAnswers = quizManager.TotalAnswers
+        resultViewcontroller.totalCorrectAnswers = quizManager.totalCorrectAnswers
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    @IBAction func selectAnsewer(_ sender: Any) {
+    @IBAction func selectAnsewer(_ sender: UIButton) {
+        let index = btAnswers.index(of: sender)!
+        quizManager.validateAnswer(index: index)
+        getNewQuiz()
     }
     
 }
